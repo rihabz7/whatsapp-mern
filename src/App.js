41456bf0d8bdd04bbfffe0db9ce85258b1c32c33
Rihab <mohamedrihaab547@gmail.com>
2021-12-13 09:35:14 +0530

    whatsapp-mern

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { useEffect }from "react";
 import './App.css';
 import Chat from "./Chat";
 import Sidebar from "./Sidebar";
+import Pusher from 'pusher-js';
 
 function App() {
+
+  useEffect(() => {
+    const pusher = new Pusher('fa8c9b02b6c71dad05d5', {
+      cluster: 'ap2'
+    });
+
+    const channel = pusher.subscribe('messages');
+    channel.bind('inserted', (data) => {
+      alert(JSON.stringify(data));
+    });
+  }, []);
+
   return (
     <div className="app">
      <div className="app__body">
